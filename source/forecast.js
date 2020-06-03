5617be06c751798c2c9bb81ae9238b939d28af31
Ankur Mohanty <ankurmoh@usc.edu>
2020-06-02 23:37:54 -0700

    Add extra forecast details

--- source/forecast.js
+++ source/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,' It is currently ' + body.current.temperature + ' degress out. The temperature feels like ' + body.current.feelslike + '. The % of rainfall is ' + body.current.precip)
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. The temperature feels like ' + body.current.feelslike + '. The % of rainfall is ' + body.current.precip + '. The wind speed currently is ' + body.current.wind_speed +' miles per hour.')
         }
     })
 }
